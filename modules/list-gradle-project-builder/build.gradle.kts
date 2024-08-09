8dbc5fdf44cdab89a442d24e7708be1948338fb4
René Link <rene.link@link-intersystems.com>
2024-08-09 05:56:07 +0200

    Changed pom description.

--- modules/list-gradle-project-builder/build.gradle.kts
+++ modules/list-gradle-project-builder/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 dependencies {
-
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     testImplementation("org.mockito:mockito-core:5.11.0")
     testImplementation("org.assertj:assertj-core:3.20.2")
@@ -14,7 +13,7 @@ publishing {
         publications.withType<MavenPublication> {
             pom {
                 name.set("Lis Gradle Project Builder")
-                description.set("Classes and utilities useful for Gradle development.")
+                description.set("Classes and utilities to build Gradle projects programmatically. Useful when writing Gradle plugin tests.")
             }
         }
     }
